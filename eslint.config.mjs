b086604a2d8ccdc3f4c7c8ddb97978b36bff0447
Kodirov2samir <kodirovsamir174@gmail.com>
2026-09-16 11:24:03 +0500

    chore: complete setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,6 +3,9 @@ import tseslint from 'typescript-eslint';
 import globals from 'globals';
 
 export default tseslint.config(
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
   {
