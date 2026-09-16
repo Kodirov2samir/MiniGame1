0f5323cd3d5a8d00a7ee0d37bdae9f85fa09a701
Kodirov2samir <kodirovsamir174@gmail.com>
2026-09-17 00:16:48 +0500

    feat: site-header completed

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
 import './styles/style.scss';
+import { siteHeader, initHeaderEvents } from './components/Header';
+const app = document.getElementById('app');
+if (app) {
+  app.innerHTML = siteHeader();
+  initHeaderEvents();
+}
