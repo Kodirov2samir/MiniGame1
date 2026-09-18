9a3459759eac5f67f12d0ab72abaea7d2799bc8b
Kodirov2samir <kodirovsamir174@gmail.com>
2026-09-19 02:53:42 +0500

    feat: add are you developer

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,14 @@ import { siteHeader, initHeaderEvents } from './components/header/Header';
 import { hero } from './components/hero/Hero';
 import { initNewGamesEvents, newGames } from './components/newGames/NewGames';
 import { topPlayers } from './components/topPlayers/topPlayers';
+import { gameDeveloper } from './components/gameDeveloper/GameDeveloper';
 const app = document.getElementById('app');
 if (app) {
   app.innerHTML = `${siteHeader()} <main>
   ${hero()}
   ${newGames()}
   ${topPlayers()}
+  ${gameDeveloper()}
   </main>`;
   initHeaderEvents();
   initNewGamesEvents();
